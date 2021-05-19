690bb8cb08b2c806080574b148bca956678e2649
Khrig <rawdonchris@googlemail.com>
2021-05-19 10:57:37 +0100

    added thermal capacity calculation

--- Chris/Thermal Analysis.py
+++ Chris/Thermal Analysis.py
@@ -19,23 +19,33 @@ def conduction(df, u_df, r_df):
     r_df["total_UA"] = r_df[["roof_UA", "wall_UA", "window_UA", "floor_UA"]].sum(axis = 1)
     return r_df
 
-def ventilation(df, u_df, r_df):
-    cp = 1.006#could calculate for marginally increased accuracy
+def ventilation(df, r_df, cp):
     hour = 60*60 # just hour in sec
     roof_area = df.groupby("name_fp")["surface_area_roof"].sum() #adding areas to find total surface area - envelope
     wall_area = df.groupby("name_fp")["surface_area_wall"].sum()
     floor_area = df.groupby("name_fp")["area_total"].nth(0)
     envelope = roof_area + wall_area + floor_area
     print(envelope)
-
+    #multiply max permissible permeability by envelope to get volume of air leaving per sec 
     r_df["ventilation_constant"] = u_df.groupby("name_fp")["permeability"].nth(0).mul(envelope)*cp/hour
-    print(r_df)
+    #divide by hour because permeability is in hours
+    return r_df, envelope
 
+def capacity(df, u_df, r_df, cp, envelope): # find heat capacity of air and walls.
+    brick_thickness = 0.1
+    df["air_cap_floorwise"] = df["volume_air"]*cp #need to times this by air density
+    brick_layer_volume = envelope*brick_thickness
+    df["wall_cap_floorwise"] = u_df["spec_wall_heat_cap"].mul(u_df["wall_density"]) #need to times this by volume
+    r_df["wall_cap"] = df.groupby("name_fp")["wall_cap_floorwise"].sum().mul(brick_layer_volume)
+    r_df["air_cap"] = df.groupby("name_fp")["air_cap_floorwise"].sum() #again, must be multiplied by air density
+    return r_df
+
+    
 
 U_value_csv_path = r"C:\Users\rawdo\Documents\year 4\project\U values.csv"
 volume_csv_path = r"C:\Users\rawdo\Documents\year 4\project\Building Plans\campus\PNGs\floor_volumes_1.0.csv"
 df = pd.read_csv(volume_csv_path, index_col = "index")
-u_df = pd.read_csv(U_value_csv_path)
+u_df = pd.read_csv(U_value_csv_path) #u values dataframe for heat conduction
 df = df.drop(columns = ["Unnamed: 0", "scale", "paper size", "area_air", "perimeter", "area_wall"])
 df["name_fp"] = df["name"].str[:-2].str.strip("_") # name from path column because original name can be inconsistent cos tesseract
 u_df["name_fp"] = u_df["name"].str[:-2].str.strip("_") # name from path column because original name can be inconsistent cos tesseract
@@ -43,13 +53,15 @@ r_df = df.groupby("name_fp").nth(0)[["area_total", "building name"]] #results da
 print(r_df)
 print(u_df)
 print(df)
-r_df = conduction(df,u_df, r_df)
+cp = 1006#could calculate for marginally increased accuracy
+
+r_df = conduction(df, u_df, r_df)
+r_df, envelope = ventilation(df, r_df, cp)
+r_df = capacity(df, u_df, r_df, cp, envelope)
+r_df.to_csv(r"C:\Users\rawdo\Documents\year 4\project\Thermal Results.csv")
 print(r_df)
-ventilation(df, u_df, r_df)
 #%%
-def thermalise(df, u_df):
-    
-    return
+
 
 
 
